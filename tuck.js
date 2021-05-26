2d07d245ea89f050cf0c583d7e3b7815d411a9d0
iluzych <ieva.daugvilaite@gmail.com>
2021-05-26 18:19:58 +0300

    Tick every second

---
+++ tuck.js
@@ -0,0 +1,20 @@
+/*
+const a = [2, 3, 1, 4, 2];
+Arejuje nurodyta, kiek sekundziu reikia laukti, kol bus isspausdinta nurodyta reiksme
+*/
+
+const a = [2, 3, 1, 4, 2];
+
+function waiting(list) {
+    const time = list[0];
+    setTimeout(() => {
+        console.log(time);
+        list.shift()
+
+        if (list.length > 0) {
+            waiting(list);
+        }
+    }, time * 1000)
+}
+
+waiting(a);
